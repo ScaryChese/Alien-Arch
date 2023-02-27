17df52b1fb8ae96705c46cb2508d0e958bae7a8b
ScaryChese <124595581+ScaryChese@users.noreply.github.com>
2023-02-27 15:57:47 +0000

    added if and arrays

--- Alien Arch - Unity/Assets/Scripts/TestScript.cs
+++ Alien Arch - Unity/Assets/Scripts/TestScript.cs
@@ -8,10 +8,59 @@ public class TestScript : MonoBehaviour
     public string startMessage;
     public string updateMessage;
 
+    public string message1;
+    public string message2;
+    public string message3;
+    public int messageChoice;
+
+    public string[] messages;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.LogWarning(startMessage);
+        //Debug.LogWarning(startMessage);
+
+
+        //messageChoice = Random.Range(1, 4);
+
+
+
+
+
+
+        /*
+       if (messageChoice == 1)
+        {
+            Debug.Log(messages[0]);
+        }
+        else if (messageChoice == 2)
+        {
+            Debug.Log(messages[1]);
+        }
+        else if (messageChoice == 3)
+        {
+            Debug.Log(messages[2]);
+        }
+        else
+        {
+            Debug.Log("Invalid message choice");
+        }
+        */
+
+        if (messages.Length - 1 >= messageChoice && messageChoice >= 0)
+        {
+            Debug.Log(messages[messageChoice]);
+        }
+        else 
+        {
+            Debug.Log("Invalid message choice");
+        }
+
+        
+
+
+
+        
     }
 
     // Update is called once per frame
