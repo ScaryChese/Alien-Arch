570ff0027125413348396c9fcb60b993a5a5ce71
ScaryChese <124595581+ScaryChese@users.noreply.github.com>
2023-05-29 15:26:17 +0100

    ai patrol mostly done

--- Alien Arch - Unity/Assets/Scripts/EnemyPatrol.cs
+++ Alien Arch - Unity/Assets/Scripts/EnemyPatrol.cs
@@ -6,18 +6,20 @@ public class EnemyPatrol : MonoBehaviour
 {
     // Public Variables
     public float forceStrength;
-    public Vector2 patrolPath; // the patrol point they will move to
+    public Vector2[] patrolPaths; // the patrol point they will move to
+    public float stopDistance; // how close we get before moving towards next point
+
 
 
     // private variables
-    private Rigidbody2D ourRigidbody;
-
+    private Rigidbody2D ourRigidBody;
+    private int currentPoint = 0; //index of current point we are moving towards
 
 
 
     void Awake()
     {
-        ourRigidbody = GetComponent<Rigidbody2D>();
+        ourRigidBody = GetComponent<Rigidbody2D>();
     }
 
 
@@ -26,10 +28,35 @@ public class EnemyPatrol : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // how far are we from target
+        float distance = (patrolPaths[currentPoint] - (Vector2)transform.position).magnitude;
+
+        // IF we are closer to our target than our minimum distance...
+        if (distance <= stopDistance)
+        {
+            // ...THEN update to next target
+            currentPoint = currentPoint + 1;
+
+            // at the end of the list
+            if (currentPoint >= patrolPaths.Length)
+            {
+                // loop back to start of list
+                currentPoint = 0;
+            }
+        }
+
+        
+
+
+
         // move in the direction of target
 
         // get direction they should move in
         //Subtracting the current position from the target position
-        Vector2 direction = (patrolPath - (Vector2)transform.position).normalized;
+        Vector2 direction = patrolPaths[currentPoint] - (Vector2)transform.position;
+        direction = direction.normalized;
+
+        // Move in the correct direction with the set force strength
+        ourRigidBody.AddForce(direction * forceStrength);
     }
 }
